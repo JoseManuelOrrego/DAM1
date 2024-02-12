f0be1afe9b66aac1557533a7b26ae0ff1a418506
josemanuelorrego <josemanuelorrego@1DAM-15>
2024-02-12 10:16:08 +0100

    jeje

--- EstructurasDeDatos/src/ejercicios/AjedrezPeones.java
+++ EstructurasDeDatos/src/ejercicios/AjedrezPeones.java
@@ -9,9 +9,12 @@ public class AjedrezPeones
 	{
 		char [][] tablero = crearAjedrez();
 		imprimirArray(tablero);
-		char x = 'a';
-		int xx = x;
-		System.out.println(xx);
+		for(int i = 0; i < 5; i++)
+		{
+			System.out.println("Turno " + (i+1));
+			intentoMovimiento(tablero);
+		}
+		System.out.println("Fin de los turnos");
 	}
 	
 	public static char[][] crearAjedrez() 
@@ -56,20 +59,51 @@ public class AjedrezPeones
 			System.out.print(" " + (tablero.length-i));
 		}
 		System.out.println("\n");
-		System.out.println("a b c d e f g h");
+		System.out.println("A B C D E F G H");
 		System.out.println();
 	}
 	
-	public static void movimiento(char[][]tablero)
+	public  static void imprimirArrayExtra(char[][]tablero, String posicionMarcada) 
 	{
-		for(int i = 0; i < 5; i++)
-		{
-			boolean bingo = false;
-			while(!bingo)
+		for(int i=0;i<tablero.length;i++) 
+		{				
+			System.out.println();				
+			for(int j = 0 ; j< tablero[i].length;j++) 
 			{
-				System.out.print("¿Qué peon quieres mover?  --> ");
-				String posicion = entrada.next();
-				String posicionConvertida = convertirPosicion(posicion);
+				if(posicionMarcada.charAt(0) - '0' == j && posicionMarcada.charAt(1) - '0' == i)
+				{
+					System.out.print("@ ");
+				}
+				else
+					System.out.print(tablero[i][j]+" ");
+			}
+			System.out.print(" " + (tablero.length-i));
+		}
+		System.out.println("\n");
+		System.out.println("A B C D E F G H				Ficha marcada con @");
+		System.out.println();
+	}
+	
+	public static void intentoMovimiento(char[][]tablero)
+	{
+		boolean bingo = false;
+		while(!bingo)
+		{
+			String posicionReal = "";
+			System.out.print("�Que peon quieres mover?  --> ");
+			String posicion = entrada.next();
+			posicionReal += Character.toUpperCase(posicion.charAt(0));
+			posicionReal += posicion.charAt(1);
+			posicion = convertirPosicion(posicionReal);
+			if(tablero[posicion.charAt(1) - '0'][posicion.charAt(0) - '0'] == 'P')
+			{
+				bingo = true;
+				imprimirArrayExtra(tablero, posicion);
+				movimientoPeon(tablero, posicion);
+			}
+			else
+			{
+				System.out.println("No se encuentra ningun peon en la posicion indicada");
 			}
 		}
 	}
@@ -77,27 +111,57 @@ public class AjedrezPeones
 	public static String convertirPosicion(String posicion)
 	{
 		String posicionConvertida = "";
-		switch(posicion.charAt(0))
-		{
-		case 'a':
-			posicionConvertida += "0";
-		case 'b':
-			posicionConvertida += "1";
-		case 'c':
-			posicionConvertida += "2";
-		case 'd':
-			posicionConvertida += "3";
-		case 'e':
-			posicionConvertida += "4";
-		case 'f':
-			posicionConvertida += "5";
-		case 'g':
-			posicionConvertida += "6";
-		case 'h':
-			posicionConvertida += "7";
-		}
+		posicionConvertida += posicion.charAt(0)- 65;
+		int segundon = posicion.charAt(1) - '0';
+		posicionConvertida += Math.abs(segundon - 8);
 		
 		return posicionConvertida;
 	}
 
+	public static void movimientoPeon(char [][] tablero, String posicion)
+	{
+		boolean cambioCorrecto = false;
+		while(!cambioCorrecto)
+		{
+			String posicionReal = "";
+			System.out.print("Inserta la casilla a la que quieres mover el peon --> ");
+			String posicionNueva = entrada.next();
+			posicionReal += Character.toUpperCase(posicionNueva.charAt(0));
+			posicionReal += posicionNueva.charAt(1);
+			posicionNueva = convertirPosicion(posicionReal);
+			
+			if(tablero[posicionNueva.charAt(1)-'0'][posicionNueva.charAt(0)-'0'] == 'X')
+			{
+				if(Math.abs((posicionNueva.charAt(1) - '0') - (posicion.charAt(1) - '0')) <= 2)
+				{
+					if(posicionNueva.charAt(0) == posicion.charAt(0))
+					{
+						cambioFichas(tablero, posicion, posicionNueva);
+						cambioCorrecto = true;
+						imprimirArray(tablero);
+					}
+					else
+					{
+						System.out.println("Tienen que ser de la misma columna");
+					}
+				}
+				else
+				{
+					System.out.println("No se puede mover mas de dos casillas");
+				}
+			}
+			else
+			{
+				System.out.println("La casilla introducida esta ocupada");
+			}
+		}
+	}
+	
+	public static boolean cambioFichas(char [][] tablero, String posicion, String posicionNueva)
+	{
+		char aux = tablero[posicionNueva.charAt(1)-'0'][posicionNueva.charAt(0)-'0'];
+		tablero[posicionNueva.charAt(1)-'0'][posicionNueva.charAt(0)-'0'] = tablero[posicion.charAt(1)-'0'][posicion.charAt(0)-'0'];
+		tablero[posicion.charAt(1)-'0'][posicion.charAt(0)-'0'] = aux;
+		return true;
+	}
 }
