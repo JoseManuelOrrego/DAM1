750ea32df846d0e750020bd6e26c8e8a6d168fb4
josemanuelorrego <josemanuelorrego@1DAM-15>
2024-02-19 10:06:09 +0100

    arrays enteros

--- EstructurasDeDatos/src/ejercicios/ArrayEnteros.java
+++ EstructurasDeDatos/src/ejercicios/ArrayEnteros.java
@@ -13,7 +13,7 @@ public class ArrayEnteros
 	
 	public ArrayEnteros(ArrayEnteros array)
 	{
-		ArrayEnteros arrayCopia = new ArrayEnteros[array];
+		ArrayEnteros arrayCopia = new ArrayEnteros(array.array.length);
 	}
 	
 	public ArrayEnteros()
@@ -31,10 +31,93 @@ public class ArrayEnteros
 				array[contador] = numero;
 			contador += 1;
 		}
+		entrada.close();
 	}
 	
-	public static void insertar()
+	public void insertar(boolean [] arrayPosiciones, int numero)
 	{
-		
+		boolean insertado = false;
+		int i = 0;
+		while(!insertado)
+		{
+			if(!arrayPosiciones[i])
+			{
+				array[i] = numero;
+				insertado = true;
+			}
+			i++;
+		}
+	}
+	
+	public void insertarWPosicion(boolean [] arrayPosiciones, int numero, int posicion)
+	{
+		if(!arrayPosiciones[posicion])
+		{
+			array[posicion] = numero;
+		}
+		else
+			System.out.println("La posicion elegida ya esta ocupada.");
+	}
+	
+	public void ordenar()
+	{
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Ordenar de mayor a menor / Ordenar de menor a mayor (1/2) --> ");
+		int opcion = entrada.nextInt();
+		if(opcion == 1)
+		{
+			int aux;
+			for(int i = 0; i < array.length-1; i++)
+			{
+				for(int j = 0; j < array.length-1-i; j++)
+				{
+					if(array[j] < array[j+1])
+					{
+						aux = array[j];
+						array[j] = array[j+1];
+						array[j+1] = aux;
+					}
+				}
+			}
+		}
+		else
+		{
+			int aux;
+			for(int i = 0; i < array.length-1; i++)
+			{
+				for(int j = 0; j < array.length-1-i; j++)
+				{
+					if(array[j] > array[j+1])
+					{
+						aux = array[j];
+						array[j] = array[j+1];
+						array[j+1] = aux;
+					}
+				}
+			}
+		}
+	}
+	
+	public boolean buscar(int buscado)
+	{
+		for(int i = 0; i < array.length; i++)
+		{
+			if(array[i] == buscado)
+				return true;
+		}
+		return false;
+	}
+	
+	public void numVeces(int buscado)
+	{
+		int numVeces = 0;
+		for(int i = 0; i < array.length; i++)
+		{
+			if(array[i] == buscado)
+			{
+				numVeces += 1;
+			}
+		}
+		System.out.println("El elemento se ha encontrado " + numVeces + " veces.");
 	}
 }
