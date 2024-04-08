16d1e18cf76ba93fcc552df0b961ad6dd4eb3406
joseo <joseo@1DAM-17>
2024-04-08 10:12:51 +0200

    dadda

---
+++ EstructurasDeDatos/src/listas/ListaEnlazada.java
@@ -0,0 +1,84 @@
+package listas;
+
+public class ListaEnlazada<T> implements Lista<T> 
+{
+
+	@Override
+	public void insertar(int posicion, T elemento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertar(T elemento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void vaciar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T get(int posicion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean estaVacia() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int indice(T elemento) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public T[] aArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T eliminar(int posicion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean eliminar(T elemento) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T set(int posicion, T elemento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int tamanno() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Lista<T> sublista(int desde, int hasta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void concatenar(Lista<T> listaNueva) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
