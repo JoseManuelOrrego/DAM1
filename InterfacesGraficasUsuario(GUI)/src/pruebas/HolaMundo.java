6334af93ae94be1d26dcd235bf3ad3a599ef6864
JoseManuelOrrego <jmobejarano@gmail.com>
2024-05-13 01:24:38 +0200

    swing comienzos

---
+++ InterfacesGraficasUsuario(GUI)/src/pruebas/HolaMundo.java
@@ -0,0 +1,30 @@
+package pruebas;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class HolaMundo {
+
+	public static void main(String[] args) 
+	{
+		miVentana mi = new miVentana();
+	}
+}
+
+class miVentana extends JFrame
+{
+	public miVentana()
+	{
+		super("Ventana");
+		setSize(500,500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		Container contenedor =  getContentPane();
+		contenedor.setLayout(new FlowLayout());
+		JLabel etiqueta = new JLabel("Hola mundo!");
+		contenedor.add(etiqueta);
+	}
+}
